9eff74eae675a0e1dcb1c08c2601e18832f7c623
MillsEasy <Dh13585507745@outlook.com>
2026-03-28 17:06:01 +0800

    Delete ThemeChanger directory

--- ThemeChanger/ThemeChanger/resource.h
+++
@@ -1,9 +0,0 @@
-#ifndef RESOURCE_H
-#define RESOURCE_H
-
-#define IDI_MYICON          103
-#define IDB_LOGO            102
-#define IDB_INSTALL_BG      104
-#define IDB_BG_WALLPAPER    105   // ÐÂ±³¾°Í¼Æ¬
-
-#endif
